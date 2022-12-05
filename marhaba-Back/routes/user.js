2fe73e8054f0b60fb611f22fafabc7bf507579ee
FATIHA99 <fatihhaa27@gmail.com>
2022-12-05 16:06:00 +0100

    Revert "every thing up to dtae"
    This reverts commit 2705f6fd851173dcf8e0ead3f4f451cf5b1277a5.

---
+++ marhaba-Back/routes/user.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const user = require('../controllers/Usercontrollers');
+const livreur = require('../controllers/Livreurcontrollers')
+const {requireSignIn} = require('../middlewares/auth');
+const {chekrole} = require('../middlewares/auth');
+const {userSignupValidator } = require('../middlewares/userValidator');
+const {userSigninValidator } = require('../middlewares/userValidator');
+const route = express.Router();
+
+
+route.post('/signup',userSignupValidator ,user.createUser);
+route.post('/signin',userSigninValidator ,user.login);
+route.get('/signout',user.signout);
+route.get('/home',requireSignIn, chekrole);
+route.post('/forgetpassword',user.forgetpassword);
+route.post('/resetpassword/:token',user.resetpassword);
+route.get('/verify/:token',user.verify);
+
+route.post('/addlivreur',userSignupValidator ,livreur.CreateLivreur)
+
+module.exports = route
