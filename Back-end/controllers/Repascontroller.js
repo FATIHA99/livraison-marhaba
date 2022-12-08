d6ef2cefe1269195f227f7bf7260a322c1251653
azeddine <maslouhazeddine@gmail.com>
2022-12-08 11:47:47 +0100

    formidable

--- Back-end/controllers/Repascontroller.js
+++ Back-end/controllers/Repascontroller.js
@@ -1,15 +1,26 @@
 const repas = require('../models/Repasmodels')
 const fdable = require('formidable')
+const http = require('http')
 
 
 const addRepas = (req,res)=>{
     const {body} = req
-    const form = new fdable.IncomingForm();
-    form.parse(req,function(err,fields,files){
-        let oldPath = files.profilePic.path;
-        let newPath = path.join(__dirname, 'uploads')
-                + '/'+files.profilePic.name
-        let rawData = fs.readFileSync(oldPath)
+    // const form = new fdable.IncomingForm();
+    // form.parse(req,function(err,fields,files){
+    //     let oldPath = files.profilePic.path;
+    //     let newPath = path.join(__dirname, 'uploads')
+    //             + '/'+files.profilePic.name
+    //     let rawData = fs.readFileSync(oldPath)
+    // })
+    http.createServer(function (req,res) {
+        if (req.url == '/fileupload') {
+            let form = new fdable.IncomingForm();
+            form.parse(req,function (err,field,files) {
+                res.send('file uploaded')
+            })
+            
+        }
+        
     })
 
     repas.create({...body}).then(e=>{
