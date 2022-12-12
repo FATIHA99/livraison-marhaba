6910ca41b591133f9adc71312c07a71c65365929
azeddine <maslouhazeddine@gmail.com>
2022-12-12 09:09:11 +0100

    add consomation front add && show

--- front-end/src/User/admin/Repas/Repas.jsx
+++ front-end/src/User/admin/Repas/Repas.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
+import axios from "axios";
 import NavAdmin from '../sideBar/navAdmin/navAdmin.jsx';
 import Sidebar from '../sideBar/Sidebar'
-import AddRepas from './AddRepas.jsx';
+import AddRepas from './AddRepas.jsx'
+import { API_URL } from "../../../config";
 
 const Repas = () => {
+    const [data, setData] = useState([])
+    const fetchRepas = () => {
+        axios.get(API_URL+'/repas').then((e)=>{
+            setData(e.data)
+        })
+      }
+    
+      useEffect(() => {
+        fetchRepas()
+      }, [])
   return (
     <main className="container-fluid bg-white">
         <div className="row d-flex flex-nowrap">
@@ -33,44 +45,27 @@ const Repas = () => {
                                     <th scope='col'>image</th>
                                     <th scope="col">name</th>
                                     <th scope="col">description</th>
+                                    <th scope='col'>price</th>
+                                    <th scope='col'>categorie</th>
                                     <th className="text-center">operation</th>
                                 </tr>
                             </thead>
                             <tbody className="bg-white">
+                                {data.map((repas)=>(
                                 <tr className="align-middle">
-                                    <th scope="row">1</th>
-                                    <th scope="row">john</th>
-                                    <td>lmalik </td>
+                                    <td>{repas.image}</td>
+                                    <td>{repas.name}</td>
+                                    <td >{repas.description}</td>
+                                    <td>{repas.price}</td>
+                                    <td>{repas.categorie}</td>
                                     <td className="d-flex flex-row justify-content-end">
-                                        <form action="" method="post">
+                                        <form className='text-nowrap' method="post">
                                             <button className='btn btn-outline-info me-1'><i class="bi bi-pencil-square"></i></button>
                                             <button className="btn btn-outline-danger"><i className="bi bi-trash"></i></button>
                                         </form>
                                     </td>
                                 </tr>
-                                <tr className="align-middle">
-                                    <th scope="row">1</th>
-                                    <th scope="row">john</th>
-                                    <td>lmalik </td>
-                                    <td className="d-flex flex-row justify-content-end">
-                                        <form action="" method="post">
-                                            <button className='btn btn-outline-info me-1'><i class="bi bi-pencil-square"></i></button>
-                                            <button className="btn btn-outline-danger"><i className="bi bi-trash"></i></button>
-                                        </form>
-                                    </td>
-                                </tr>
-                                <tr className="align-middle">
-                                    <th scope="row">1</th>
-                                    <th scope="row">john</th>
-                                    <td>lmalik </td>
-                                    <td className="d-flex flex-row justify-content-end">
-                                        <form action="" method="post">
-                                            <button className='btn btn-outline-info me-1'><i class="bi bi-pencil-square"></i></button>
-                                            <button className="btn btn-outline-danger"><i className="bi bi-trash"></i></button>
-                                        </form>
-                                    </td>
-                                </tr>
-                              
+                                ))}                            
                             </tbody>
                         </table>
                     </div>
